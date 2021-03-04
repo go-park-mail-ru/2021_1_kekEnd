63d3aaf8d407680b7c336cd47d43393e3f8a6b2b
Алексей <zotov.alexei.1@yandex.ru>
2021-03-05 01:38:23 +0300

    auth usecase

--- auth/usecase.go
+++ auth/usecase.go
@@ -5,5 +5,5 @@ import "context"
 type UseCase interface {
 	SignUp(ctx context.Context, username, email, password string) error
 
-	Login(ctx context.Context, login, password string) (bool, error)
+	Login(ctx context.Context, login, password string) bool
 }
--- auth/usecase/usecase.go
+++ auth/usecase/usecase.go
@@ -1,3 +1,37 @@
 package usecase
 
+import (
+	"context"
+	"github.com/go-park-mail-ru/2021_1_kekEnd/kekEnd_main/auth"
+	"github.com/go-park-mail-ru/2021_1_kekEnd/kekEnd_main/models"
+)
 
+type AuthUseCase struct {
+	userRepository auth.UserRepository
+}
+
+func NewAuthUseCase(repo auth.UserRepository) *AuthUseCase {
+	return &AuthUseCase{
+		userRepository: repo,
+	}
+}
+
+func (authUC *AuthUseCase) SignUp(ctx context.Context, username, email, password string) error {
+	// хэширование и соль
+
+	user := &models.User{
+		Username: username,
+		Email: email,
+		Password: password,
+	}
+
+	return authUC.userRepository.CreateUser(ctx, user);
+}
+
+func (authUC *AuthUseCase) Login(ctx context.Context, login, password string) bool {
+	_, err := authUC.userRepository.GetUser(ctx, login, password)
+	if err != nil {
+		return false
+	}
+	return true
+}
