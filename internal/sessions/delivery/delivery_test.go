c735e8de46d9eea93a4f16220ffa28772e3bb17c
Gleb Bakhmetyev <bakhmetyevgv@gmail.com>
2021-03-10 22:18:53 +0300

    Session Delivery.Create test

--- internal/sessions/delivery/delivery_test.go
+++ internal/sessions/delivery/delivery_test.go
@@ -1,1 +1,54 @@
 package sessions
+
+import (
+	"context"
+	"errors"
+	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/sessions"
+	"github.com/golang/mock/gomock"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	testErr := errors.New("error in Create Session")
+
+	t.Run("Create-OK", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		username := "whaevaforeva"
+		UUID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Create(context.TODO(), username, 123123).
+			Return(UUID, nil)
+
+		sessionID, err := delivery.Create(context.TODO(), username, 123123)
+		assert.NoError(t, err)
+		assert.Equal(t, UUID, sessionID)
+	})
+
+	t.Run("Create-Error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		username := "whaevaforeva"
+		UUID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Create(context.TODO(), username, 123123).
+			Return(UUID, testErr)
+
+		_, err := delivery.Create(context.TODO(), username, 12323)
+		assert.Error(t, err)
+	})
+}
