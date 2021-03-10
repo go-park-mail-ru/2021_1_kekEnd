26ccfa1173eadfef48d7c107b31c2f72985698ff
Gleb Bakhmetyev <bakhmetyevgv@gmail.com>
2021-03-10 23:46:06 +0300

    Session Delivery Tests

--- internal/sessions/delivery/delivery_test.go
+++ internal/sessions/delivery/delivery_test.go
@@ -1,143 +1,141 @@
 package sessions
-//
-//import (
-//	"context"
-//	"errors"
-//	"fmt"
-//	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/sessions"
-//	"github.com/golang/mock/gomock"
-//	uuid "github.com/satori/go.uuid"
-//	"github.com/stretchr/testify/assert"
-//	"testing"
-//)
-//
-//func TestCreate(t *testing.T) {
-//	testErr := errors.New("error in Create Session")
-//
-//	t.Run("Create-OK", func(t *testing.T) {
-//		ctrl := gomock.NewController(t)
-//		defer ctrl.Finish()
-//
-//		mUC := sessions.NewMockUseCase(ctrl)
-//		delivery := NewDelivery(mUC)
-//
-//		username := "whaevaforeva"
-//		UUID := uuid.NewV4().String()
-//
-//		mUC.
-//			EXPECT().
-//			Create(context.TODO(), username, 123123).
-//			Return(UUID, nil)
-//
-//		sessionID, err := delivery.Create(context.TODO(), username, 123123)
-//		assert.NoError(t, err)
-//		assert.Equal(t, UUID, sessionID)
-//	})
-//
-//	t.Run("Create-Error", func(t *testing.T) {
-//		ctrl := gomock.NewController(t)
-//		defer ctrl.Finish()
-//
-//		mUC := sessions.NewMockUseCase(ctrl)
-//		delivery := NewDelivery(mUC)
-//
-//		username := "whaevaforeva"
-//		UUID := uuid.NewV4().String()
-//
-//		mUC.
-//			EXPECT().
-//			Create(context.TODO(), username, 123123).
-//			Return(UUID, testErr)
-//
-//		_, err := delivery.Create(context.TODO(), username, 12323)
-//		assert.Error(t, err)
-//	})
-//}
-//
-//func TestGetUser(t *testing.T) {
-//	testErr := errors.New("error in GetUser from Session")
-//
-//	t.Run("GetUser-OK", func(t *testing.T) {
-//		ctrl := gomock.NewController(t)
-//		defer ctrl.Finish()
-//
-//		mUC := sessions.NewMockUseCase(ctrl)
-//		delivery := NewDelivery(mUC)
-//
-//		username := "whaevaforeva"
-//		sessionID := uuid.NewV4().String()
-//
-//		mUC.
-//			EXPECT().
-//			Check(context.TODO(), sessionID).
-//			Return(username, nil)
-//
-//		userFromSession, err := delivery.GetUser(context.TODO(), sessionID)
-//
-//		assert.NoError(t, err)
-//		assert.Equal(t, username, userFromSession)
-//	})
-//
-//	t.Run("GetUser-Error", func(t *testing.T) {
-//		ctrl := gomock.NewController(t)
-//		defer ctrl.Finish()
-//
-//		mUC := sessions.NewMockUseCase(ctrl)
-//		delivery := NewDelivery(mUC)
-//
-//		username := "whaevaforeva"
-//		sessionID := uuid.NewV4().String()
-//
-//		mUC.
-//			EXPECT().
-//			Check(context.TODO(), sessionID).
-//			Return(username, testErr)
-//
-//		_, err := delivery.GetUser(context.TODO(), sessionID)
-//
-//		assert.Error(t, err)
-//	})
-//}
-//
-//func TestDelete(t *testing.T) {
-//	testErr := errors.New("error in Delete Session")
-//
-//	t.Run("Delete-OK", func(t *testing.T) {
-//		ctrl := gomock.NewController(t)
-//		defer ctrl.Finish()
-//
-//		mUC := sessions.NewMockUseCase(ctrl)
-//		delivery := NewDelivery(mUC)
-//
-//		sessionID := uuid.NewV4().String()
-//
-//		mUC.
-//			EXPECT().
-//			Delete(context.TODO(), sessionID).
-//			Return(nil)
-//
-//		err := delivery.Delete(context.TODO(), sessionID)
-//
-//		assert.NoError(t, err)
-//	})
-//
-//	t.Run("Delete-Error", func(t *testing.T) {
-//		ctrl := gomock.NewController(t)
-//		defer ctrl.Finish()
-//
-//		mUC := sessions.NewMockUseCase(ctrl)
-//		delivery := NewDelivery(mUC)
-//
-//		username := "whaevaforeva"
-//		sessionID := uuid.NewV4().String()
-//
-//		mUC.
-//			EXPECT().
-//			Check(context.TODO(), sessionID).
-//			Return(username, testErr)
-//
-//		_, err := delivery.GetUser(context.TODO(), sessionID)
-//
-//		assert.Error(t, err)
-//	})
-//}
+
+import (
+	"errors"
+	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/sessions"
+	"github.com/golang/mock/gomock"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreate(t *testing.T) {
+	testErr := errors.New("error in Create Session")
+
+	t.Run("Create-OK", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		username := "whaevaforeva"
+		UUID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Create(username, 123123).
+			Return(UUID, nil)
+
+		sessionID, err := delivery.Create(username, 123123)
+		assert.NoError(t, err)
+		assert.Equal(t, UUID, sessionID)
+	})
+
+	t.Run("Create-Error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		username := "whaevaforeva"
+		UUID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Create(username, 123123).
+			Return(UUID, testErr)
+
+		_, err := delivery.Create(username, 12323)
+		assert.Error(t, err)
+	})
+}
+
+func TestGetUser(t *testing.T) {
+	testErr := errors.New("error in GetUser from Session")
+
+	t.Run("GetUser-OK", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		username := "whaevaforeva"
+		sessionID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Check(sessionID).
+			Return(username, nil)
+
+		userFromSession, err := delivery.GetUser(sessionID)
+
+		assert.NoError(t, err)
+		assert.Equal(t, username, userFromSession)
+	})
+
+	t.Run("GetUser-Error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		username := "whaevaforeva"
+		sessionID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Check(sessionID).
+			Return(username, testErr)
+
+		_, err := delivery.GetUser(sessionID)
+
+		assert.Error(t, err)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	testErr := errors.New("error in Delete Session")
+
+	t.Run("Delete-OK", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		sessionID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Delete(sessionID).
+			Return(nil)
+
+		err := delivery.Delete(sessionID)
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("Delete-Error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		mUC := sessions.NewMockUseCase(ctrl)
+		delivery := NewDelivery(mUC)
+
+		username := "whaevaforeva"
+		sessionID := uuid.NewV4().String()
+
+		mUC.
+			EXPECT().
+			Check(sessionID).
+			Return(username, testErr)
+
+		_, err := delivery.GetUser(sessionID)
+
+		assert.Error(t, err)
+	})
+}
