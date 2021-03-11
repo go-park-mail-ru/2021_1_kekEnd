3e18ea7e22a53f4fbd8aaa4f0287115857583836
Алексей <zotov.alexei.1@yandex.ru>
2021-03-11 11:17:38 +0300

    fix usecase tests

--- internal/users/usecase/usecase_test.go
+++ internal/users/usecase/usecase_test.go
@@ -18,26 +18,34 @@ func TestUsersUseCase(t *testing.T) {
 		Password:      "1234",
 	}
 
-	repo.On("GetUserByUsername", user.Username).Return(nil, errors.New("user not found")).Once()
-	repo.On("CreateUser", user).Return(nil)
-	err := uc.CreateUser(user)
-	assert.NoError(t, err)
+	t.Run("CreateUser", func(t *testing.T) {
+		repo.On("GetUserByUsername", user.Username).Return(nil, errors.New("user not found")).Once()
+		repo.On("CreateUser", user).Return(nil)
+		err := uc.CreateUser(user)
+		assert.NoError(t, err)
+	})
 
-	repo.On("GetUserByUsername", user.Username).Return(user, nil)
-	repo.On("CheckPassword", user.Password, user).Return(true, nil)
-	success := uc.Login(user.Username, user.Password)
-	assert.True(t, success)
+	t.Run("LoginUser", func(t *testing.T) {
+		repo.On("GetUserByUsername", user.Username).Return(user, nil)
+		repo.On("CheckPassword", user.Password, user).Return(true, nil)
+		success := uc.Login(user.Username, user.Password)
+		assert.True(t, success)
+	})
 
-	gotUser, err := uc.GetUser(user.Username)
-	assert.NoError(t, err)
-	assert.Equal(t, user, gotUser)
+	t.Run("GetUser", func(t *testing.T) {
+		gotUser, err := uc.GetUser(user.Username)
+		assert.NoError(t, err)
+		assert.Equal(t, user, gotUser)
+	})
 
-	updatedUser := models.User{
-		Username:      "let_robots_reign",
-		Email:         "corrected@ya.ru",
-		Password:      "1234567",
-	}
-	repo.On("UpdateUser", user, updatedUser).Return(&updatedUser, nil)
-	_, err = uc.UpdateUser(user, updatedUser)
-	assert.NoError(t, err)
+	t.Run("UpdateUser", func(t *testing.T) {
+		updatedUser := models.User{
+			Username:      "let_robots_reign",
+			Email:         "corrected@ya.ru",
+			Password:      "1234567",
+		}
+		repo.On("UpdateUser", user, updatedUser).Return(&updatedUser, nil)
+		_, err := uc.UpdateUser(user, updatedUser)
+		assert.NoError(t, err)
+	})
 }
