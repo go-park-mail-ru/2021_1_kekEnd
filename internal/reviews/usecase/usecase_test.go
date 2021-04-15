d1862410e0485ea95f02635493b2f5d58e2d41ca
Alexey Zotov <zotov.alexei.1@yandex.ru>
2021-04-15 15:48:14 +0300

    users tests

--- internal/reviews/usecase/usecase_test.go
+++ internal/reviews/usecase/usecase_test.go
@@ -1,2 +1,25 @@
 package usecase
 
+import (
+	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/models"
+	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/reviews/mocks"
+	"github.com/golang/mock/gomock"
+	"testing"
+)
+
+func TestReviewsUseCase(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	
+	repo := mocks.NewMockReviewRepository(ctrl)
+	uc := NewReviewsUseCase(repo)
+	
+	review := &models.Review{
+		ID:         "1",
+		Title:      "Review",
+		ReviewType: "positive",
+		Content:    "test",
+		Author:     "let_robots_reign",
+		MovieID:    "1",
+	}
+}
--- internal/users/repository.go
+++ internal/users/repository.go
@@ -4,6 +4,7 @@ import (
 	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/models"
 )
 
+//go:generate mockgen -destination=mocks/repository.go -package=mocks . UserRepository
 type UserRepository interface {
 	CreateUser(user *models.User) error
 
