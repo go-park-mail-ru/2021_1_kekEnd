dc45721519df121d8dd2d1b39200252d4eb717dc
Gleb Bakhmetyev <bakhmetyevgv@gmail.com>
2021-04-15 16:32:48 +0300

    Add status codes in csrf middleware

--- internal/middleware/csrf.go
+++ internal/middleware/csrf.go
@@ -3,10 +3,11 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/logger"
+	"net/http"
 )
 
 type Csrf interface {
-	CheckCsrf() gin.HandlerFunc
+	CheckCSRF() gin.HandlerFunc
 }
 
 type CsrfMiddleware struct {
@@ -19,19 +20,21 @@ func NewCsrfMiddleware(Log *logger.Logger) *CsrfMiddleware {
 	}
 }
 
-func (m *CsrfMiddleware) CheckCsrf() gin.HandlerFunc {
+func (m *CsrfMiddleware) CheckCSRF() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		Token := ctx.GetHeader("X-CSRF-Token")
 		Cookie, err := ctx.Cookie("X-CSRF-Cookie")
 		if err != nil {
 			msg := "No csrf cookie in request" + err.Error()
 			m.Log.LogWarning(ctx, "CsrfMiddleware", "CheckCSRF", msg)
+			ctx.Status(http.StatusBadRequest) // 400
 			return
 		}
 
 		if Token != Cookie {
 			msg := "Csrf-Cookie doesn't match Csrf-Token"
 			m.Log.LogWarning(ctx, "CsrfMiddleware", "CheckCSRF", msg)
+			ctx.Status(http.StatusForbidden) // 403
 			return
 		}
 
