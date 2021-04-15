e90a47ff74a5439c7fb54dfaac3f2ef1e8380135
Alexey Zotov <zotov.alexei.1@yandex.ru>
2021-04-15 19:33:34 +0300

    one test for actor

---
+++ internal/actors/usecase/usecase_test.go
@@ -0,0 +1,29 @@
+package usecase
+
+import (
+	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/actors/mocks"
+	"github.com/go-park-mail-ru/2021_1_kekEnd/internal/models"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestActorsUseCase(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	repo := mocks.NewMockRepository(ctrl)
+	uc := NewActorsUseCase(repo)
+	
+	actor := models.Actor{
+		ID:   "1",
+		Name: "Tom Cruise",
+	}
+
+	t.Run("GetActor", func(t *testing.T) {
+		repo.EXPECT().GetActorByID(actor.ID).Return(actor, nil)
+		gotActor, err := uc.GetActor(actor.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, actor, gotActor)
+	})
+}
